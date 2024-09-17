9a38845a5a243d38c73c61fb4941757d4ebfcc71
Islam Kiyasov <islam7407@gmail.com>
2024-09-17 16:29:34 +0200

    1.3.11

--- example/src/app.service.ts
+++ example/src/app.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
 
 @Injectable()
 export class AppService {
+  constructor(private moduleRef: ModuleRef) {
+    console.log(moduleRef);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
